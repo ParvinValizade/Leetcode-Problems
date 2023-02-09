de16844c6fe8a51f500392090ca1f9ec9cee11ac
ParvinValizade <parvin.valizade@mail.ru>
2023-02-09 11:21:58 +0400

    2 Two Sum

---
+++ src/main/java/problems/TwoSum.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2,7,11,15};
+         int[] result = twoSum(nums,9);
+        for (int j : result) System.out.println(j);
+    }
+}
