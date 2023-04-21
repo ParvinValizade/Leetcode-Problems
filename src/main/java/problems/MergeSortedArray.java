8ff0fc2601ce9500c2e016ff46b2f1273784379f
ParvinValizade <parvin.valizade@mail.ru>
2023-04-21 17:55:31 +0400

    Array

--- src/main/java/problems/MergeSortedArray.java
+++ src/main/java/problems/MergeSortedArray.java
@@ -1,11 +1,6 @@
 package problems;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MergeSortedArray {
     public static int [] merge(int[] nums1, int m, int[] nums2, int n) {
